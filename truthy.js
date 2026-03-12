42922b466821c2405949f1a3ebbd70da8675062b
Zim <mdmohtasimbillahzim@gmail.com>
2026-03-12 19:29:28 +0600

    some extra feature of dom

---
+++ truthy.js
@@ -0,0 +1,22 @@
+let data;
+data=0;
+data='';
+data='0';
+data=false;
+data=true;
+data=null;
+data=undefined;
+data=[];
+data={};
+if(data){
+    console.log('Data is truthy');
+}else{
+    console.log('Data is falsy');
+}
+
+let price=0;
+if(!price){
+    console.log('Price is truthy');
+}else{
+    console.log('Price is falsy');
+}         
